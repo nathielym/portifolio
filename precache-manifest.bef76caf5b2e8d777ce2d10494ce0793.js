88f61dac68cb9467ae7ba5fa8db9738f3bd69fa2
Nathiely Macedo <nathiely@alunos.utfpr.edu.br>
2018-11-08 17:32:59 -0200

    Updates

---
+++ precache-manifest.bef76caf5b2e8d777ce2d10494ce0793.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0013cd873cc9df630226",
+    "url": "/portifolio/static/css/main.a0544b3a.chunk.css"
+  },
+  {
+    "revision": "0013cd873cc9df630226",
+    "url": "/portifolio/static/js/main.0013cd87.chunk.js"
+  },
+  {
+    "revision": "087139b49fd92afb4133",
+    "url": "/portifolio/static/js/1.087139b4.chunk.js"
+  },
+  {
+    "revision": "02a436588872a4a66e58",
+    "url": "/portifolio/static/js/runtime~main.02a43658.js"
+  },
+  {
+    "revision": "ff3fe8a3064f87cd9868081c88a7de66",
+    "url": "/portifolio/static/media/tec1.ff3fe8a3.svg"
+  },
+  {
+    "revision": "170e2f4c2dcbf0ec40243b986f77903f",
+    "url": "/portifolio/static/media/tec2.170e2f4c.svg"
+  },
+  {
+    "revision": "e70ba3dbf3dd5f8423e5471cfb5fa68a",
+    "url": "/portifolio/static/media/tec3.e70ba3db.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portifolio/static/media/git.76d344f0.svg"
+  },
+  {
+    "revision": "f4c2b29bd9993ed857675ad8c4d2d333",
+    "url": "/portifolio/static/media/fb.f4c2b29b.svg"
+  },
+  {
+    "revision": "3dc3acc3b8005c79d81c3058d52310f8",
+    "url": "/portifolio/static/media/avatar.3dc3acc3.svg"
+  },
+  {
+    "revision": "7f48441f77af899c81436752d631e7e6",
+    "url": "/portifolio/static/media/curriculo.7f48441f.pdf"
+  },
+  {
+    "revision": "5108d04c66153cd818768d6dccb57d91",
+    "url": "/portifolio/index.html"
+  }
+];
